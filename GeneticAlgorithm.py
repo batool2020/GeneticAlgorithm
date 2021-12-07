207131c5f8a90fcb63def81f4757fa1b606adbd4
batool2020 <73987807+batool2020@users.noreply.github.com>
2021-12-07 23:26:38 +0200

    Update GeneticAlgorithm.py

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,6 +1,6 @@
 import random
 from array import *
-from typing import List
+from typing import List, Optional, Callable, Tuple
 
 # Creates a list containing 36 lists, each of 3 items, all set to 0
 p = 38
@@ -11,6 +11,7 @@ List = [[0 for x in range(r)] for y in range(c)]
 Chromosome = []
 Population = [[ Chromosome ]] * size
 choiceMatrix: []
+fitness=0;
 
 def read_selections() -> Matrix:
  
@@ -42,12 +43,14 @@ def generate_Chromosome(length: int) -> Chromosome:  #need to pass list as argum
     projects = [x for x in range(p)]
     random.shuffle(projects)
     ch1 = projects[:36]
+    print("Final CH1 \n", ch1)
     return ch1
 
 
 
 def generate_population(size: int) -> Population:
     return [generate_Chromosome(r) for i in range(size)]
+
 
 def fitness(chromosome: Chromosome, choiceMatrix: List) -> int:
     score: int = 0
@@ -78,18 +81,65 @@ def mutation(ch1: Chromosome, num: int = 1, probability: float = 0.5) -> Chromos
             temp = ch1[index1]
             ch1[index1] = ch1[index2]
             ch1[index2] = temp
+    
 
     return ch1
 
 
 
+def selection_pair(population: Population, list: List)   -> [Chromosome, Chromosome]:   # we can pass fitness function instead of list
+    fitnessList = []
+    temp = []
+    maxnum1 =0;
+    maxnum2 = 0;
+    for i in range(size):
+        print(population[i], " Fitness is : ", fitness(population[i],list))
+        fitnessList.append(fitness(population[i],list))
 
-print(fitness(generate_Chromosome(36), convert_to_list(read_selections())))
-#Chromosome = generate_Chromosome(36)
-#print("Final Choromose", Chromosome)
+    temp = fitnessList.copy()
+    maxnum1 = max(temp)
+    temp.remove(maxnum1)
+    maxnum2 = max(temp)
+    print ("Max 1  is", population[fitnessList.index(maxnum1)] , "\n" , "Max 2 is ", population[fitnessList.index(maxnum2)])
+    
+    
+    return  population[fitnessList.index(maxnum1)] ,  population[fitnessList.index(maxnum2)]
 
-Population = generate_population(size)
+ 
+def run_evolution(population: Population, list: List, fitness_limit: int,generation_limit :int) -> [Population, int]:
+    
+    fitness_limit = 0;
+
+
+    population = generate_population(size)
+
+    for i in range(generation_limit):
+        # compare fitnesses and return the top 2 solutions \ if their fitenss is the best of all then break - need to figure it out - does sorted work? 
+       # population = sorted(population,key=fitness(population[i],list), reverse= True)
+
+        for x in range(size):
+            if fitness(population[x], list) >= fitness_limit:
+                print("should return the best 2 chromosomes")
+       
+        #if fitness(population[0],list) >= fitness_limit:
+           # break
+
+        #next_generation = population[0:2]
+
+        for j in range(int(len(population) / 2 ) - 1):
+            parents = selection_pair(generate_population(size),convert_to_list(Matrix))
+            ch1, ch2 = crossover(parents[0], parents[1])
+            ch1 = mutation(ch1)
+            ch2 = mutation(ch2)
+            next_generation += [ch1][ch2]
+
+        population = next_generation
+    population = sorted(population, fitness(population[i],list), true)
+
+    return population, i
 
 
 
 
+selection_pair(generate_population(size),convert_to_list(Matrix))
+run_evolution(generate_population(size),convert_to_list(Matrix),15, 6)
